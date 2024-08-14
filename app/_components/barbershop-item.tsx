5169aa69b9886405e2355e807be88c7b1c91f476
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2024-08-13 21:58:09 -0600

    feat: create initial barbershop page

--- app/_components/barbershop-item.tsx
+++ app/_components/barbershop-item.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import { Button } from "./ui/button"
 import { Badge } from "./ui/badge"
 import { StarIcon } from "lucide-react"
+import Link from "next/link"
 
 interface BarbershopItemProps {
   barbershop: Barbershop
@@ -33,8 +34,12 @@ export function BarbershopItem({ barbershop }: BarbershopItemProps) {
         <div className="px-1 py-3">
           <h3 className="truncate font-semibold">{barbershop.name}</h3>
           <p className="truncate text-sm text-gray-400">{barbershop.address}</p>
-          <Button variant="secondary" className="mt-3 w-full capitalize">
-            Book now
+          <Button
+            variant="secondary"
+            className="mt-3 w-full capitalize"
+            asChild
+          >
+            <Link href={`/barbershops/${barbershop.id}`}>Book now</Link>
           </Button>
         </div>
       </CardContent>
