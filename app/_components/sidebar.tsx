db1d3f2fb188e083ea98134a4b9ab35dbe432284
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2024-08-25 06:36:11 -0600

    feat: add authentication schema and Google provider with Next Auth

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,10 +1,17 @@
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { Button } from "./ui/button"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
-import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import { quickSearchOptions } from "../_constants/search"
 import Image from "next/image"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 export function Sidebar() {
   return (
@@ -13,15 +20,41 @@ export function Sidebar() {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
       {/* AVATAR */}
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Hello! Please log in.</h2>
+
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Log In</DialogTitle>
+              <DialogDescription>
+                Sign in with your Google account.
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-1 font-bold">
+              <Image
+                alt="Sign in with your Google account"
+                src="/google.svg"
+                width={18}
+                height={18}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+
+        {/* <Avatar>
           <AvatarImage src="https://images.unsplash.com/photo-1706885093487-7eda37b48a60?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         </Avatar>
-
         <div>
           <p className="font-bold">Jane Doe</p>
           <p className="text-xs">jane@test.com</p>
-        </div>
+        </div> */}
       </div>
 
       {/* HOME & APPOINTMENTS */}
